261811408014c4fb1383a9cd3c634a92970d7f91
Denis-Zenkov <Denis-olegov@mail.ru>
2022-05-23 16:00:19 +0300

    Init1

--- src/main/java/com/example/pp_3_1_4_1/controller/AdminController.java
+++ src/main/java/com/example/pp_3_1_4_1/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.example.pp_3_1_4_1.controller;
 
 import com.example.pp_3_1_4_1.model.User;
 import com.example.pp_3_1_4_1.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,27 +19,30 @@ public final class AdminController {
     }
 
     @GetMapping("/users")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK) ;
     }
 
     @GetMapping("users/{id}")
-    public User getUserById(@PathVariable long id) {
+    public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
     @PostMapping("/users")
-    public User addUser(@RequestBody User user) {
-        return userService.addUser(user);
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        userService.addUser(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PutMapping("/users")
-    public User updateUser(@RequestBody User user) {
-        return userService.updateUser(user);
+    public ResponseEntity<User> updateUser(@RequestBody User user/*,
+                                           @PathVariable Long id*/) {
+        userService.updateUser(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable long id) {
+    public void deleteUser(@PathVariable Long id) {
         userService.deleteById(id);
     }
 }
